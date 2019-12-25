62f1a7b0a41b1e6be57b67b8793ad8fe175e7c7a
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2019-12-25 20:35:11 +0100

    Add Nat

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = do
-  parseResult <- parseFile "example.pie"
-  case parseResult of
-    Right result -> print (filter notComment result)
-    Left error -> print error
+main = go
--- src/Lib.hs
+++ src/Lib.hs
@@ -10,11 +10,27 @@ data Expression
   = Atom String
   | Comment Expression
   | Pair Expression Expression
-  deriving (Show)
+  | Zero
+  | Add1 Expression
+
+instance Show Expression where
+  show (Atom x) = x
+  show (Comment _) = ""
+  show (Pair x y) = "Pair[" <> show x <> "," <> show  y  <> "]"
+  show Zero = show $ foldNat Zero
+  show (Add1 x) = (show . foldNat . Add1) x
+
+foldNat :: Expression -> Int
+foldNat (Add1 x) = 1 + foldNat x
+foldNat Zero = 0
+foldNat _ = 0
 
 notComment :: Expression -> Bool
-notComment (Comment x) = False
+notComment (Comment _) = False
 notComment _ = True
+
+number :: PieParser () Int
+number = read <$> many1 digit
 
 comment :: PieParser () Expression
 comment = do
@@ -46,14 +62,41 @@ car = do
   (Pair x _) <- invocation
   return x
 
+add1 :: PieParser () Expression
+add1 =
+    between (char '(') (char ')') add1'
+    where
+        add1' = do
+            _ <- string "add1 "
+            Add1 <$> (zero <|> add1)
+
+zero :: PieParser () Expression
+zero = do
+    _ <- string "zero"
+    return Zero
+
+nat :: PieParser () Expression
+nat =
+  zero <|> add1
+
 invocation :: PieParser () Expression
-invocation = between (char '(') (char ')') (try cons <|> try cdr <|> try car)
+invocation = between
+                (char '(')
+                (char ')')
+                (try cons <|> try cdr <|> try car)
 
 expression :: PieParser () Expression
-expression = atom <|> comment <|> invocation
+expression = try atom <|> try comment <|> try invocation <|> try nat
 
 expressions :: PieParser () [Expression]
 expressions = sepBy expression newline
 
 parseFile :: String -> IO (Either ParseError [Expression])
 parseFile = parseFromFile expressions
+
+go :: IO ()
+go = do
+  parseResult <- parseFile "example.pie"
+  case parseResult of
+    Right result -> print (filter notComment result)
+    Left emsg -> print emsg
